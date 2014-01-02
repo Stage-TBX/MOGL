228ec7edf4644bd3dbb4e24716bbeeb1a9df6d3a
Mark Cafaro <mark.cafaro@gmail.com>
2014-01-02 15:39:58 -0800

    Compiled for Windows 64bit

--- make.m
+++ make.m
@@ -1,8 +1,4 @@
 function make()
-    if ~ismac
-        error('This function is currently Mac only');
-    end
-
     filePath = mfilename('fullpath');
     moglDir = fileparts(filePath);
     
@@ -10,5 +6,9 @@ function make()
     returnToDir = onCleanup(@()cd(currentDir));
     cd(fullfile(moglDir, 'source'));
     
-    mex -v -outdir ./ -output moglcore -DMACOSX -DGLEW_STATIC -largeArrayDims -f ../mexopts.sh LDFLAGS="\$LDFLAGS -framework OpenGL -framework GLUT" -I/usr/include moglcore.c gl_auto.c gl_manual.c glew.c mogl_rebinder.c ftglesGlue.c
+    if ismac
+        mex -v -outdir ./ -output moglcore -DMACOSX -DGLEW_STATIC -largeArrayDims -f ../mexopts.sh LDFLAGS="\$LDFLAGS -framework OpenGL -framework GLUT" -I/usr/include moglcore.c gl_auto.c gl_manual.c glew.c mogl_rebinder.c ftglesGlue.c;
+    elseif ispc
+        mex -v -outdir ./ -output moglcore -DWINDOWS -DGLEW_STATIC -largeArrayDims -L../freeglut/lib/x64 -lfreeglut -I../freeglut/include windowhacks.c moglcore.c gl_auto.c gl_manual.c glew.c mogl_rebinder.c ftglesGlue.c user32.lib gdi32.lib advapi32.lib glu32.lib opengl32.lib;
+    end
 end
--- source/glew.c
+++ source/glew.c
@@ -29,12 +29,15 @@
 ** THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-#include <GL/glew.h>
+/* MK glew.h, glxew.h, wglew.h includes modified for Psychtoolbox PTB: #include <GL/glew.h> */
+#include "glew.h"
 
 #if defined(_WIN32)
-#  include <GL/wglew.h>
-#elif !defined(__ANDROID__) && !defined(__native_client__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
-#  include <GL/glxew.h>
+/* #  include <GL/wglew.h> */
+#  include "wglew.h"
+#elif !defined(__ANDROID__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
+/* #  include <GL/glxew.h> */
+#  include "glxew.h"
 #endif
 
 /*
